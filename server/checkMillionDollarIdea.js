dc032149d13dc42ccf77fbf4c6c33684688d5881
chpiotr06 <chpiotr06@gmail.com>
2022-09-10 13:53:05 +0200

    passing all tests for ideas-router and checkMillionDollarIdea

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,4 +1,13 @@
-const checkMillionDollarIdea = () => {};
+const checkMillionDollarIdea = (req, res, next) => {
+  const { numWeeks, weeklyRevenue } = req.body;
+  let total;
+  total = numWeeks * weeklyRevenue;
+  if(total >= 1000000) {
+    next()
+  } else {
+    res.sendStatus(400)
+  } 
+};
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
--- server/ideas-router.js
+++ server/ideas-router.js
@@ -1,5 +1,69 @@
 const express = require('express');
 const ideasRouter = express.Router();
 
+const { getAllFromDatabase, getFromDatabaseById, addToDatabase, updateInstanceInDatabase, deleteFromDatabasebyId } = require('./db')
+const checkMillionDollarIdea = require('./checkMillionDollarIdea')
+
+const extractData = (req, res, next) => {
+  const { id, name, description, numWeeks, weeklyRevenue } = req.body;
+  req.idea = {id, name, description, numWeeks, weeklyRevenue};
+  next()
+}
+
+const isUniqueId = (req, res, next) => {
+  const { id } = req.body;
+  const isUnique = getFromDatabaseById('ideas', id);
+  req.isUniqueId = isUnique;
+  next();
+}
+
+ideasRouter.get('/', (req, res, next) => {
+  const ideas = getAllFromDatabase('ideas');
+  res.send(ideas);
+});
+
+ideasRouter.get('/:ideaId', (req, res, next) => {
+  const idea = getFromDatabaseById('ideas', req.params.ideaId)
+  if(idea) {
+    res.send(idea);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+ideasRouter.put('/:ideaId', extractData, isUniqueId, (req, res, next) => {
+  if(req.isUniqueId) {
+    const updatedIdea = updateInstanceInDatabase('ideas', req.idea);
+    if(updatedIdea){
+      res.send(updatedIdea);
+    } else {
+      res.sendStatus(404);
+    }
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+ideasRouter.post('/', checkMillionDollarIdea, extractData, isUniqueId, (req, res, next) => {
+  if(!req.isUniqueId){
+    const newIdea = addToDatabase('ideas', req.idea);
+    if(newIdea){
+      res.status(201).send(newIdea);
+    } else {
+      res.sendStatus(404);
+    }
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+ideasRouter.delete('/:ideaId', (req, res, next) => {
+  const hasDeleted = deleteFromDatabasebyId('ideas', req.params.ideaId);
+  if(hasDeleted) {
+    res.sendStatus(204)
+  } else {
+    res.sendStatus(404);
+  }
+})
 
 module.exports = ideasRouter;
