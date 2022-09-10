5fb0d19e151a724b4a4124077f7a35d200fc3383
chpiotr06 <chpiotr06@gmail.com>
2022-09-10 14:13:15 +0200

    Passing all test for meetings-router

--- server/meetings-router.js
+++ server/meetings-router.js
@@ -1,5 +1,28 @@
 const express = require('express');
 const meetingsRouter = express.Router();
 
+const { getAllFromDatabase, addToDatabase, deleteAllFromDatabase } = require('./db')
+
+meetingsRouter.get('/', (req, res, next) => {
+  const meetings = getAllFromDatabase('meetings');
+  res.send(meetings);
+})
+meetingsRouter.post('/', (req, res, next) => {
+  const { time, date, day, note } = req.body;
+  const newMeeting = addToDatabase('meetings', {
+    time:`${time}`,
+    date: new Date(date), 
+    day: `${day}`, 
+    note: `${note}`});
+  if(newMeeting){
+    res.status(201).send(newMeeting);
+  } else {
+    res.sendStatus(404);
+  }
+})
+meetingsRouter.delete('/', (req, res, next) => {
+  deleteAllFromDatabase('meetings');
+  res.sendStatus(204);
+})
 
 module.exports = meetingsRouter;
